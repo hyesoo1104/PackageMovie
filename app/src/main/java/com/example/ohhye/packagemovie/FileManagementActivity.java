a290384f96066db4e805a0151e100c7a2e3b19b4
hyesoo1104 <hyesoo1104@gmail.com>
2015-02-21 00:57:02 +0900

    파일메뉴 타이틀 구현

--- app/src/main/java/com/example/ohhye/packagemovie/FileManagementActivity.java
+++ app/src/main/java/com/example/ohhye/packagemovie/FileManagementActivity.java
@@ -2,16 +2,32 @@ package com.example.ohhye.packagemovie;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 /**
  * Created by ohhye on 2015-01-26.
  */
-public class FileManagementActivity  extends ActionBarActivity {
+public class FileManagementActivity  extends ActionBarActivity implements View.OnClickListener {
+
+    private ImageView btn_file_add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_file);
+
+        btn_file_add = (ImageView)findViewById(R.id.btn_file_add);
+        btn_file_add.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_file_add:
+            break;
+
+        }
+    }
 }
