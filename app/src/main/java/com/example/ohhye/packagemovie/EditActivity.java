b844913cbcdbfbc3200d9a53edf878a142734fb3
hyesoo1104 <hyesoo1104@gmail.com>
2015-02-21 03:50:18 +0900

    편집메뉴 헤더 구현완료

--- app/src/main/java/com/example/ohhye/packagemovie/EditActivity.java
+++ app/src/main/java/com/example/ohhye/packagemovie/EditActivity.java
@@ -1,7 +1,9 @@
 package com.example.ohhye.packagemovie;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.WindowManager;
 
 /**
  * Created by ohhye on 2015-01-26.
@@ -11,6 +13,8 @@ public class EditActivity  extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main_edit);
     }
 
