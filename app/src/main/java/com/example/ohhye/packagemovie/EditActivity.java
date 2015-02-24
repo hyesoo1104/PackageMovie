dfc4643f787a82a3f7d8eee1e48ffd53db6d5915
ohhye <hyesoo1104@gmail.com>
2015-02-24 17:34:04 +0900

    편집메뉴 구현

--- app/src/main/java/com/example/ohhye/packagemovie/EditActivity.java
+++ app/src/main/java/com/example/ohhye/packagemovie/EditActivity.java
@@ -1,9 +1,7 @@
 package com.example.ohhye.packagemovie;
 
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.WindowManager;
 
 /**
  * Created by ohhye on 2015-01-26.
@@ -13,8 +11,6 @@ public class EditActivity  extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main_edit);
     }
 
