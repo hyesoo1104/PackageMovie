3dec949a86c1036a3876ef7c5139dd9e2e904e54
ohhye <hyesoo1104@gmail.com>
2015-02-26 02:46:42 +0900

    mp3파일리스트

--- app/src/main/java/com/example/ohhye/packagemovie/Edit_AddBGM.java
+++ app/src/main/java/com/example/ohhye/packagemovie/Edit_AddBGM.java
@@ -1,109 +1,54 @@
 package com.example.ohhye.packagemovie;
 
 import android.app.Activity;
-import android.content.Context;
+import android.content.ContentResolver;
 import android.database.Cursor;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.BaseAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
-
-import java.io.File;
-import java.io.FilenameFilter;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 /**
  * Created by ohhye on 2015-02-26.
  */
 
-class MP3Filter implements FilenameFilter{
-    public boolean accept(File dir, String name){
-        return (name.endsWith(".mp3"));   //확장자가 mp3인지 확인
-    }
-}
-
-
 public class Edit_AddBGM extends Activity {
-    private Cursor audiocursor;
-    private int audio_column_index;
-    ListView audiolist;
-    int count;
+    ContentResolver contentResolver;
+    MediaPlayer mediaPlayer;
 
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    Cursor cursor;
+    ListView listView;
+
+    public void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_edit_add_bgm);
-        init_phone_audio_grid();
+
+        listView = (ListView)findViewById(R.id.edit_add_bgm_list);
+
+        contentResolver = getContentResolver();
+        cursor = contentResolver.query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,null,null,null,null);
+
+        SimpleCursorAdapter simpleCursorAdapter =
+                new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, cursor,
+                                        new String[]{MediaStore.MediaColumns.DISPLAY_NAME},
+                                        new int[]{android.R.id.text1});
+
+        listView.setAdapter(simpleCursorAdapter);
+        listView.setOnItemClickListener(onItemClickListener);
+
+        startManagingCursor(cursor);
     }
 
-    private void init_phone_audio_grid() {
-        System.gc();
-        String[] proj = { MediaStore.Audio.Media._ID,
-                MediaStore.Audio.Media.DISPLAY_NAME,
-                MediaStore.Audio.Media.SIZE };
-        audiocursor = managedQuery(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
-                proj, null, null, null);
-        count = audiocursor.getCount();
-        audiolist = (ListView) findViewById(R.id.PhoneAudioList);
-        audiolist.setAdapter(new audioAdapter(getApplicationContext()));
-        audiolist.setOnItemClickListener(audiogridlistener);
-    }
-
-    private OnItemClickListener audiogridlistener = new OnItemClickListener() {
+    private OnItemClickListener onItemClickListener = new OnItemClickListener() {
         public void onItemClick(AdapterView parent, View v, int position,
                                 long id) {
-            System.gc();
-            audio_column_index = audiocursor
-                    .getColumnIndexOrThrow(MediaStore.Audio.Media.DATA);
-            audiocursor.moveToPosition(position);
-//            String filename = audiocursor.getString(audio_column_index);
-//            Intent intent = new Intent(Edit_AddBGM.this, Viewaudio.class);
-//            intent.putExtra("audiofilename", filename);
-//            startActivity(intent);
+            Toast.makeText(Edit_AddBGM.this,"click",Toast.LENGTH_SHORT).show();
         }
     };
 
-    public class audioAdapter extends BaseAdapter {
-        private Context vContext;
-
-        public audioAdapter(Context c) {
-            vContext = c;
-        }
-
-        public int getCount() {
-            return count;
-        }
-
-        public Object getItem(int position) {
-            return position;
-        }
-
-        public long getItemId(int position) {
-            return position;
-        }
-
-        public View getView(int position, View convertView, ViewGroup parent) {
-            System.gc();
-            TextView tv = new TextView(vContext.getApplicationContext());
-            String id = null;
-            if (convertView == null) {
-                audio_column_index = audiocursor
-                        .getColumnIndexOrThrow(MediaStore.Audio.Media.DISPLAY_NAME);
-                audiocursor.moveToPosition(position);
-                id = audiocursor.getString(audio_column_index);
-                audio_column_index = audiocursor
-                        .getColumnIndexOrThrow(MediaStore.Audio.Media.SIZE);
-                audiocursor.moveToPosition(position);
-                tv.setText(id);
-            } else
-                tv = (TextView) convertView;
-
-            return tv;
-        }
-    }
 }
